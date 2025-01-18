418aaf7401e70ded72d8ed7e22223f85a80e06dd
chishige1217200 <daichicchi01@gmail.com>
2025-01-18 13:56:17 +0900

    Add: JSDocを記載

--- api/index.js
+++ api/index.js
@@ -1,17 +1,28 @@
 const express = require("express");
-const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require("express-validator");
 const cors = require("cors");
 const path = require("path");
 
-// JSONを読み込む
+/**
+ * クイズデータが格納されているファイル名
+ */
 const fileName = "it-quiz-v1.6.0.json";
+
+/**
+ * クイズデータ
+ */
 const quizData = require(path.resolve("./data/" + fileName));
 // console.log(quizData)
 
-// 処理共通化用のEnum
+/**
+ * 処理共通化用のEnum
+ * @enum {number}
+ */
 const responseMode = Object.freeze({ question: 0, answer: 1 });
 
-// expressインスタンスの作成
+/**
+ * expressインスタンス
+ */
 const app = express();
 
 // CORS設定
@@ -20,11 +31,9 @@ app.use(cors());
 // JSONボディをパースするミドルウェア
 app.use(express.json());
 
-// エントリポイントの定義
-app.get("/", (req, res) => {
-  res.send({ message: "Hello, World." });
-});
-
+/**
+ * sendWebhook処理用のバリデーション
+ */
 const postValidation = [
   // number型の0以上チェック
   body("id")
@@ -46,6 +55,11 @@ const postValidation = [
     .withMessage("webhook_url is required."),
 ];
 
+// エントリポイントの定義
+app.get("/", (req, res) => {
+  res.send({ message: "Hello, World." });
+});
+
 app.post("/send_question", postValidation, (req, res) => {
   sendWebhook(req, res, responseMode.question);
 });
@@ -57,8 +71,20 @@ app.post("/send_answer", postValidation, (req, res) => {
 // サーバの開始
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
-function sendWebhook(req, res, returnMode) {
+/**
+ * Webhookを送信する処理
+ * @param {*} req
+ * @param {*} res
+ * @param {responseMode} responseMode - レスポンスモード
+ * @returns {void}
+ */
+function sendWebhook(req, res, responseMode) {
+  /**
+   * バリデーション結果
+   */
   const errors = validationResult(req);
+
+  // バリデーションでエラーが発生した場合の処理
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
