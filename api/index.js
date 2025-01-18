caa867f0ce67da2f8f14a7c6249328e8ac021da8
chishige1217200 <daichicchi01@gmail.com>
2025-01-18 12:34:50 +0900

    Add: JSONの読み込み処理

--- api/index.js
+++ api/index.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const cors = require("cors");
-const fs = require("fs");
 const path = require("path");
 
+// JSONを読み込む
+const fileName = "it-quiz-v1.6.0.json"
+const quizData = require(path.resolve("./data/" + fileName));
+// console.log(quizData)
+
+// expressインスタンスの作成
 const app = express();
 
+// CORS設定
 app.use(cors());
 
+// エントリポイントの定義
 app.get("/", (req, res) => {
   res.send({ message: "Hello, World." });
 });
 
+// サーバの開始
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
